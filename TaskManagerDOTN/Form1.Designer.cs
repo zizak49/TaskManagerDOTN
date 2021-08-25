f1abe11a920510278b8e02b845d5cdc7153aafc3
Mislav <mislav.franja@gmail.com>
2021-08-25 10:59:16 +0200

    populate data grid view with processes

--- TaskManagerDOTN/Form1.Designer.cs
+++ TaskManagerDOTN/Form1.Designer.cs
@@ -29,13 +29,101 @@ namespace TaskManagerDOTN
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Processes = new System.Windows.Forms.TabPage();
+            this.processName = new System.Windows.Forms.Label();
+            this.processesDataGridView = new System.Windows.Forms.DataGridView();
+            this.Preformance = new System.Windows.Forms.TabPage();
+            this.Services = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.Processes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.processesDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Processes);
+            this.tabControl1.Controls.Add(this.Preformance);
+            this.tabControl1.Controls.Add(this.Services);
+            this.tabControl1.Location = new System.Drawing.Point(13, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 437);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Processes
+            // 
+            this.Processes.Controls.Add(this.processName);
+            this.Processes.Controls.Add(this.processesDataGridView);
+            this.Processes.Location = new System.Drawing.Point(4, 24);
+            this.Processes.Name = "Processes";
+            this.Processes.Padding = new System.Windows.Forms.Padding(3);
+            this.Processes.Size = new System.Drawing.Size(767, 409);
+            this.Processes.TabIndex = 0;
+            this.Processes.Text = "Processes";
+            this.Processes.UseVisualStyleBackColor = true;
+            // 
+            // processName
+            // 
+            this.processName.AutoSize = true;
+            this.processName.Location = new System.Drawing.Point(3, 45);
+            this.processName.Name = "processName";
+            this.processName.Size = new System.Drawing.Size(39, 15);
+            this.processName.TabIndex = 1;
+            this.processName.Text = "Name";
+            // 
+            // processesDataGridView
+            // 
+            this.processesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.processesDataGridView.Location = new System.Drawing.Point(3, 63);
+            this.processesDataGridView.Name = "processesDataGridView";
+            this.processesDataGridView.RowTemplate.Height = 25;
+            this.processesDataGridView.Size = new System.Drawing.Size(761, 343);
+            this.processesDataGridView.TabIndex = 0;
+            // 
+            // Preformance
+            // 
+            this.Preformance.Location = new System.Drawing.Point(4, 24);
+            this.Preformance.Name = "Preformance";
+            this.Preformance.Padding = new System.Windows.Forms.Padding(3);
+            this.Preformance.Size = new System.Drawing.Size(767, 409);
+            this.Preformance.TabIndex = 1;
+            this.Preformance.Text = "Preformance";
+            this.Preformance.UseVisualStyleBackColor = true;
+            // 
+            // Services
+            // 
+            this.Services.Location = new System.Drawing.Point(4, 24);
+            this.Services.Name = "Services";
+            this.Services.Size = new System.Drawing.Size(767, 409);
+            this.Services.TabIndex = 2;
+            this.Services.Text = "Services";
+            this.Services.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.Processes.ResumeLayout(false);
+            this.Processes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.processesDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Processes;
+        private System.Windows.Forms.TabPage Preformance;
+        private System.Windows.Forms.TabPage Services;
+        private System.Windows.Forms.Label processName;
+        private System.Windows.Forms.DataGridView processesDataGridView;
     }
 }
 
--- TaskManagerDOTN/Form1.cs
+++ TaskManagerDOTN/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace TaskManagerDOTN
 {
     public partial class Form1 : Form
     {
+        public Process[] processes;
+
         public Form1()
         {
             InitializeComponent();
+            LoadAllProcesses();
+        }
+
+        private void LoadAllProcesses() 
+        {
+            processes = Process.GetProcesses();
+            processesDataGridView.Columns.Add("Name","Name");
+            foreach (Process process in processes)
+            {
+                processesDataGridView.Rows.Add(process.ProcessName);
+            }
         }
 
     }
