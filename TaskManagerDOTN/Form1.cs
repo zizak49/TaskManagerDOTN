8fbed8a68e57bf708258fad6576f129a95945ba4
Mislav <mislav.franja@gmail.com>
2021-08-27 10:32:32 +0200

    added total memory of PC and for every process

--- TaskManagerDOTN/Form1.cs
+++ TaskManagerDOTN/Form1.cs
@@ -8,17 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using Microsoft.Win32;
+using Microsoft.VisualBasic.Devices;
 
 namespace TaskManagerDOTN
 {
     public partial class Form1 : Form
     {
         public Process[] processes;
+        private ComputerInfo computerInfo;
+        ulong totalMemory;
 
         public Form1()
         {
             InitializeComponent();
             LoadAllProcesses();
+            computerInfo = new ComputerInfo(); 
+            LoadSystemInformation();
         }
 
         private void LoadAllProcesses() 
@@ -29,9 +35,31 @@ namespace TaskManagerDOTN
 
             for (int i = 0; i < processes.Length-1; i++)
             {
+                //Proces names
                 processesDataGridView.Rows.Add(processes[i].ProcessName);
-                processesDataGridView.Rows[i].Cells[1].Value = processes[i].PagedMemorySize64;
-            }          
+
+                //Process memory usage
+                long x = processes[i].PagedMemorySize64;
+
+                for (int j = 0; j < 2; j++)
+                {
+                    x /= 1024;
+                }
+
+                processesDataGridView.Rows[i].Cells[1].Value = x + " MB";
+            }
+        }
+
+        private void LoadSystemInformation() 
+        {
+            totalMemory = computerInfo.TotalPhysicalMemory;
+
+            for (int i = 0; i < 2; i++)
+            {
+                totalMemory = totalMemory / 1024;
+            }
+
+            totalMemory = totalMemory / 1000;
         }
 
     }
